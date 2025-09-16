efd53a04b06333dca5cc472f25195387ad2780dd
Adrian Markefka <am97veb@gmail.com>
2025-09-16 13:39:38 +0200

    Added GlobalStyles, Normalize, and ThemeProvider to main.tsx. Index.css has been removed.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./core/App.tsx";
+import { GlobalStyle } from "./core/GlobalStyles.ts";
+import { Normalize } from "styled-normalize";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+    <Normalize />
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    downriver: "#0D2555",
+  },
+  breakpoints: {
+    
+  },
+} as const ;
