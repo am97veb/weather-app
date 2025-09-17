61828fc239135109ed12cd677eaf4968a8846ca4
Adrian Markefka <am97veb@gmail.com>
2025-09-17 15:51:10 +0200

    added QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,20 @@ import { GlobalStyle } from "./core/GlobalStyles.ts";
 import { Normalize } from "styled-normalize";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-    <Normalize />
-    <GlobalStyle />
-    <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <ThemeProvider theme={theme}>
+        <Normalize />
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
