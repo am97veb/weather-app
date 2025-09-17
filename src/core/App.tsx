4df13163c59b1870c779bea8eaeee1b16863b00e
Adrian Markefka <am97veb@gmail.com>
2025-09-17 16:59:43 +0200

    added CurrentWeather component to Route

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,12 +1,13 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { toCurrentWeather } from "./routes";
+import { CurrentWeather } from "../features/currentWeather";
 import { Navigation } from "./Navigation";
 
 const App = () => (
   <HashRouter>
     <Navigation />
     <Routes>
-      <Route path={toCurrentWeather()} />
+      <Route path={toCurrentWeather()} element={<CurrentWeather />} />
       <Route path="/" element={<Navigate to={toCurrentWeather()} />} />
     </Routes>
   </HashRouter>
