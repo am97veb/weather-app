b754736543ce5bfa15cf76539d77ab46d7e47192
Adrian Markefka <am97veb@gmail.com>
2025-09-18 10:22:00 +0200

    theme updated

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,26 @@
 export const theme = {
   colors: {
     downriver: "#0D2555",
+    dodgerBlue: "#28ADF6",
+    black: "#000000",
+    boxShadow: "#000000",
+    white: "#FFFFFF",
+    starDust: "#9F9F9D"
   },
   breakpoints: {
-    
+    sm: 576,
+    md: 768,
+    lg: 992,
+
+
   },
+  fonts: {
+    sm: 18,
+    md: 22,
+    lg: 24,
+    xl: 28,
+    xxl: 34,
+    xxxl: 46,
+    huge: 60,
+  }
 } as const ;
