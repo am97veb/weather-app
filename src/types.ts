d47faa936760f7dd21e6d246b77b1b44923d71c2
Adrian Markefka <am97veb@gmail.com>
2025-09-19 17:27:27 +0200

    types added

--- src/types.ts
+++ src/types.ts
@@ -3,11 +3,27 @@ export interface CityDataProps {
     name: string;
     country: string;
   };
-  current: {
-    temp_c: number;
-    condition: {
-      text: string;
-      icon: string;
-    };
+  forecast?: WeatherForecast;
+  current: CurrentWeather;
+}
+
+export interface CurrentWeather {
+  temp_c: number;
+  condition: ConditionProps;
+}
+export interface ConditionProps {
+  text: string;
+  icon: string;
+}
+
+export interface ForecastDay {
+  date: string;
+  day: {
+    maxtemp_c: number;
+    mintemp_c: number;
+    condition: ConditionProps;
   };
+}
+export interface WeatherForecast {
+  forecastday: ForecastDay[];
 }
