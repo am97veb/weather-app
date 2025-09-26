3ddfa452e8e73688460ef7a0ed9912301316e241
Adrian Markefka <am97veb@gmail.com>
2025-09-26 12:44:01 +0200

    removed logic from component and improved api link - now it is generic

--- src/SourceApi.tsx
+++ src/SourceApi.tsx
@@ -1,9 +1,3 @@
 import { API_KEY, API_URL } from "./apiElements";
 
-export const SourceApi = (city: string, pathname: string) => {
-
-  if (pathname === "/currentWeather") {
-    return `${API_URL}current.json?key=${API_KEY}&q=${city}&aqi=no`;
-  }
-  return `${API_URL}forecast.json?key=${API_KEY}&q=${city}&days=7`;
-};
+export const sourceApi = (city: string, endpoint: string) => `${API_URL}${endpoint}?key=${API_KEY}&q=${city}`;
