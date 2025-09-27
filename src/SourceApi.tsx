bd177165ef03013318c05eca80a7f71bd1e60e00
Adrian Markefka <am97veb@gmail.com>
2025-09-27 10:41:55 +0200

    refactoring - used axios instance to build URL instead of manual concatenation

--- src/SourceApi.tsx
+++
@@ -1,3 +0,0 @@
-import { API_KEY, API_URL } from "./apiElements";
-
-export const sourceApi = (city: string, endpoint: string) => `${API_URL}${endpoint}?key=${API_KEY}&q=${city}`;
--- src/fetchApiData.tsx
+++ src/fetchApiData.tsx
@@ -1,8 +1,20 @@
 import axios from "axios";
-import { sourceApi } from "./sourceApi";
 import { FetchDataProps } from "./types";
+import { API_KEY, API_URL } from "./apiElements";
+
+const weatherApi = axios.create({
+  baseURL: API_URL,
+  params: {
+    key: API_KEY,
+  },
+});
 
 export const fetchApiData = async (city: string, endpoint: string) => {
-    const response = await axios.get<FetchDataProps>(sourceApi(city, endpoint));
-    return response.data; 
+  const response = await weatherApi.get<FetchDataProps>(endpoint, {
+    params: { 
+        q: city,
+        days: "3",
+    },
+  });
+  return response.data;
 };
