159256c028c1461e7219006a9a953a414c51cc5d
Adrian Markefka <am97veb@gmail.com>
2025-10-10 17:45:28 +0200

    new date formatting functions added

--- src/common/dataFormatter.tsx
+++ src/common/dataFormatter.tsx
@@ -1,5 +1,17 @@
-export const dateFormatter = (date: string) =>
-    new Date(date).
-        toLocaleDateString("en-EN", {
-            weekday: "long",
-        });
+export const dateToWeekday = (date: string) =>
+  new Date(date).toLocaleDateString("en-EN", {
+    weekday: "long",
+  });
+
+export const dateToHour = (date: string) =>
+  new Date(date).toLocaleTimeString(undefined, {
+    hour: "numeric",
+  });
+
+export const dateToCompare = (date: string | Date) =>
+  new Date(date).toLocaleTimeString(undefined, {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "numeric",
+  });
