1d0b4d4f785427775a3756bbc5bef5b827b9b46f
Adrian Markefka <am97veb@gmail.com>
2025-10-17 14:20:48 +0200

    function for modifying property names has been created

---
+++ src/features/propertyNameModifier.tsx
@@ -0,0 +1,15 @@
+export const propertyNameModifier = (property: string, title: string) => {
+  switch (true) {
+    case title.includes("air"):
+      return property
+        .toUpperCase()
+        .replace("GB-DEFRA-INDEX", "Daily Air Quality Index")
+        .replace("US-EPA-INDEX", "US EPA Index")
+        .replaceAll("_", ".");
+    case title.includes("sun and moon"):
+      return property.replaceAll("_", " ");
+
+    default:
+      return property;
+  }
+};
